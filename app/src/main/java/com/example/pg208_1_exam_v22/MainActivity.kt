d38db83d8bc8a1188a1ca6fef13185bb828d359f
mikailqureshi <miqu001@student.kristiania.no>
2022-04-18 15:07:40 +0200

    Project start

---
+++ app/src/main/java/com/example/pg208_1_exam_v22/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pg208_1_exam_v22
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+} 
---
+++ app/src/test/java/com/example/pg208_1_exam_v22/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.pg208_1_exam_v22
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
